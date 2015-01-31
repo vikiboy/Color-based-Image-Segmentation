c33fd6d3ad81112c568f9610231d0f70d3a5ab52
Vikram Mohanty <vikram.mohanty.1993@gmail.com>
2015-01-31 14:52:24 +0530

    2nd Commit Version

---
+++ CMakeFiles/IGVC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/IGVC.dir/obstacle_segmentation.cpp.o"
+  "IGVC.pdb"
+  "IGVC"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/IGVC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
